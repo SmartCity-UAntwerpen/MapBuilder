bfdc3bc9418f14c1c40d831b015f141ff08cfe51
Riad <riad.seffine@gmail.com>
2020-01-14 13:44:40 +0100

    Update generating of sql-script for the robots:   - support for tlights is created

--- sql.js
+++ sql.js
@@ -14,7 +14,8 @@ var robot_tablename_tile = "tiles";
 var robot_tablename_point = "points";
 var robot_tablename_link_lock = "link_locks";
 var robot_tablename_link = "links";
-
+var robot_tablename_tlight = "tlights";
+var tlights_default_state = "green";
 
 function displaySQL(){
   $("#topSql").val(getSQL_top())
@@ -82,53 +83,74 @@ function getSQL_drone(){
 
 function getSQL_robot(){
 
-  var sql = "USE " + robot_databasename + ";";
+  var sql = "USE " + robot_databasename + ";\n\n";
 
-  sql += "SET foreign_key_checks=0;";
+  sql += "SET foreign_key_checks=0;\n";
   sql += "CREATE TABLE " + robot_databasename + "." + robot_tablename_link +
       "(id bigint not null, angle double precision not null, length integer not null, " +
       "weight integer not null, end bigint, link_lock_id bigint, start bigint, " +
-      "primary key (id)) ENGINE=InnoDB;";
+      "primary key (id)) ENGINE=InnoDB;\n";
   sql += "CREATE TABLE " + robot_databasename + "." + robot_tablename_link_lock +
       "(id bigint not null, status bit, locked_by bigint, " +
-      "primary key(id)) ENGINE=InnoDB;";
+      "primary key(id)) ENGINE=InnoDB;\n";
   sql += "CREATE TABLE " + robot_databasename + "." + robot_tablename_point +
       "(id bigint not null, is_locked bit, rfid varchar(255), type varchar(255), " +
-      "locked_by bigint, primary key(id)) ENGINE=InnoDB;";
+      "locked_by bigint, primary key(id)) ENGINE=InnoDB;\n";
   sql += "CREATE TABLE " + robot_databasename + "." + robot_tablename_tile +
-      "(id bigint not null, tile_id bigint, primary key (id)) ENGINE=InnoDB;";
-  sql += "TRUNCATE " + robot_databasename + "." + robot_tablename_link;
-  sql += "TRUNCATE " + robot_databasename + "." + robot_tablename_link_lock;
-  sql += "TRUNCATE " + robot_databasename + "." + robot_tablename_point;
-  sql += "TRUNCATE " + robot_databasename + "." + robot_tablename_tile;
-  sql += "SET foreign_key_checks=1;";
+      "(id bigint not null, tile_id bigint, primary key (id)) ENGINE=InnoDB;\n";
+  sql += "CREATE TABLE " + robot_databasename + "." + robot_tablename_tlight +
+      "(id bigint not null, state varchar(255), point_id bigint, primary key(id)) ENGINE=InnoDB;\n";
+  sql += "TRUNCATE " + robot_databasename + "." + robot_tablename_link + "\n";
+  sql += "TRUNCATE " + robot_databasename + "." + robot_tablename_link_lock + "\n";
+  sql += "TRUNCATE " + robot_databasename + "." + robot_tablename_point + "\n";
+  sql += "TRUNCATE " + robot_databasename + "." + robot_tablename_tile + "\n";
+  sql += "TRUNCATE " + robot_databasename + "." + robot_tablename_tlight + "\n";
+  sql += "SET foreign_key_checks=1;\n\n";
 
   //tiles
   var tiles = [];
+  var tlights = [];
   $.each(robot_tiles, function(key,values){
     if(values.isTile){
       tiles.push(values)
     }
-  })
+  });
 
   $.each(tiles, function(key,values){
     sql += "INSERT INTO " + robot_databasename + "." + robot_tablename_tile + "(id, rfid, is_locked, tiles.type) VALUES(" +
-            values.tileId + ",\"" + values.rfid + "\"," + 0 + ",\"" + values.tiletype + "\");\n"
-  })
+            values.tileId + ",\"" + values.rfid + "\"," + 0 + ",\"" + values.tiletype + "\");\n";
+    if(values.tiletype.toLowerCase().trim() === "tlight")
+      tlights.push(values);
+  });
 
   //nodes - only nodes who have links
-  var used_nodes = []
+  var used_nodes = [];
   $.each(robot_links, function(key,values){
     used_nodes.push(values.start);
     used_nodes.push(values.stop);
-  })
+  });
 
+  var tlightNodes = new Map();
   $.each(robot_nodes, function(key,values){
     if(used_nodes.includes(values.id)){
       sql += "INSERT INTO " + robot_databasename + "." + robot_tablename_point + "(id, tile_id) VALUES(" +
-            values.id + "," + values.tileId + ");\n"
+            values.id + "," + values.tileId + ");\n";
+      $.each(tlights, function(k, v)
+      {
+         if(v.tileId === values.tileId)
+           tlightNodes.set(values.tileId, values.id);
+      });
     }
-  })
+  });
+
+  //tlights
+
+  $.each(tlights, function(key, values)
+  {
+      sql += "INSERT INTO " + robot_databasename + "." + robot_tablename_tlight + "(id, state, point_id) VALUES(" +
+          key + ",\"" + tlights_default_state + "\"," + tlightNodes.get(values.tileId) + ");\n";
+
+  });
 
 
   //links
